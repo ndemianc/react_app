1d5b9227ea1ecc23e279d004fbc46d3952082305
Sergii Demianchuk <sdemian@softserveinc.com>
2016-04-12 01:53:39 +0300

    Allow to drop only on the valid path. Finish rook entity.

--- src/components/board_square.js
+++ src/components/board_square.js
@@ -4,38 +4,11 @@ import { canMoveRook, moveRook } from './game';
 import { ItemTypes } from '../constants/items_types';
 import { DropTarget } from 'react-dnd';
 
-class BoardSquare extends Component {
-  render() {
-    const { x, y, connectDropTarget, isOver } = this.props;
-    const black = (x + y) % 2 === 1;
-
-    return connectDropTarget(
-      <div style={{
-          position: 'relative',
-          width: '100%',
-          height: '100%'
-        }}>
-        <Square black={black}>
-          {this.props.children}
-        </Square>
-        {isOver &&
-          <div style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              height: '100%',
-              width: '100%',
-              zIndex: 1,
-              opacity: 0.5,
-              backgroundColor: 'yellow'
-            }}/>
-        }
-      </div>
-    );
-  }
-}
-
 const squareTarget = {
+  canDrop(props) {
+    return canMoveRook(props.x, props.y);
+  },
+
   drop(props, monitor) {
     moveRook(props.x, props.y);
   }
@@ -44,14 +17,49 @@ const squareTarget = {
 function collect(connect, monitor) {
   return {
     connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver()
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  }
+}
+
+class BoardSquare extends Component {
+  renderOverlay(color) {
+    return (
+      <div style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          height: '100%',
+          width: '100%',
+          zIndex: 1,
+          opacity: 0.5,
+          backgroundColor: color
+        }} />
+    );
+  }
+
+  render() {
+    const { x, y, connectDropTarget, isOver, canDrop } = this.props;
+    const black = (x + y) % 2 === 1;
+
+    return connectDropTarget(
+      <div style={{ position: 'relative' }}>
+        <Square black={black}>
+          {this.props.children}
+        </Square>
+        {isOver && !canDrop && this.renderOverlay('red')}
+        {!isOver && canDrop && this.renderOverlay('yellow')}
+        {isOver && canDrop && this.renderOverlay('green')}
+      </div>
+    );
   }
 }
 
 BoardSquare.propTypes = {
   x: PropTypes.number.isRequired,
   y: PropTypes.number.isRequired,
-  isOver: PropTypes.bool.isRequired
+  isOver: PropTypes.bool.isRequired,
+  canDrop: PropTypes.bool.isRequired
 }
 
 export default DropTarget(ItemTypes.ROOK, squareTarget, collect)(BoardSquare);
